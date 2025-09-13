c38a6f5ad7248a887c035d295bb141fe95c19798
studybuild576-commits <studybuild576@gmail.com>
2025-09-13 12:19:37 +0530

    Update enemy.dart

--- lib/components/enemy.dart
+++ lib/components/enemy.dart
@@ -3,8 +3,12 @@ import 'package:flame/components.dart';
 import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
 
+import '../game.dart';
+
 // Dushman Component
-class Enemy extends PositionComponent with CollisionCallbacks {
+class Enemy extends PositionComponent with HasGameRef<ChronoshotGame>, CollisionCallbacks {
+  final double _speed = 150; // Dushman ki speed
+
   Enemy({required Vector2 position}) : super(position: position);
 
   @override
@@ -13,6 +17,16 @@ class Enemy extends PositionComponent with CollisionCallbacks {
     size = Vector2.all(40.0);
     anchor = Anchor.center;
     add(RectangleHitbox());
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    // Player ki taraf badhne ka logic
+    if (gameRef.player.isMounted) {
+      final direction = (gameRef.player.position - position).normalized();
+      position.add(direction * _speed * dt);
+    }
   }
 
   @override

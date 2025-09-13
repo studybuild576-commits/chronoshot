ebaa7369bfcf0c3167a4fb1c42b2b97593531970
studybuild576-commits <studybuild576@gmail.com>
2025-09-13 15:36:07 +0530

    Create bullet.dart

---
+++ lib/components/bullet.dart
@@ -0,0 +1,35 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flame/palette.dart';
+import 'package:flutter/material.dart';
+
+import '../game.dart';
+
+class Enemy extends PositionComponent with HasGameRef<ChronoshotGame>, CollisionCallbacks {
+  final double _speed = 150;
+
+  Enemy({required Vector2 position}) : super(position: position);
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    size = Vector2.all(40.0);
+    anchor = Anchor.center;
+    add(RectangleHitbox());
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (gameRef.player.isMounted) {
+      final direction = (gameRef.player.position - position).normalized();
+      position.add(direction * _speed * dt);
+    }
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    canvas.drawRect(size.toRect(), BasicPalette.red.paint());
+  }
+}

acd13559475ef44e3fb22f7dcdbfbc03b013b854
studybuild576-commits <studybuild576@gmail.com>
2025-09-13 16:34:23 +0530

    Update bullet.dart

--- lib/components/bullet.dart
+++ lib/components/bullet.dart
@@ -1,19 +1,21 @@
-import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/collisions.dart';
 import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
 
 import '../game.dart';
+import 'enemy.dart';
 
-class Enemy extends PositionComponent with HasGameRef<ChronoshotGame>, CollisionCallbacks {
-  final double _speed = 150;
+class Bullet extends PositionComponent
+    with HasGameRef<ChronoshotGame>, CollisionCallbacks {
+  final double _speed = 500;
 
-  Enemy({required Vector2 position}) : super(position: position);
+  Bullet({required Vector2 position}) : super(position: position);
 
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    size = Vector2.all(40.0);
+    size = Vector2.all(10.0);
     anchor = Anchor.center;
     add(RectangleHitbox());
   }
@@ -21,15 +23,29 @@ class Enemy extends PositionComponent with HasGameRef<ChronoshotGame>, Collision
   @override
   void update(double dt) {
     super.update(dt);
-    if (gameRef.player.isMounted) {
-      final direction = (gameRef.player.position - position).normalized();
-      position.add(direction * _speed * dt);
+    position.add(Vector2(0, -1) * _speed * dt);
+
+    // Remove bullet if out of screen
+    if (position.y < 0) {
+      removeFromParent();
+    }
+
+    // Collision check with enemies
+    final enemies = gameRef.children.whereType<Enemy>().toList();
+    for (var enemy in enemies) {
+      if (enemy.toRect().overlaps(toRect())) {
+        enemy.removeFromParent();
+        removeFromParent();
+        break;
+      }
     }
   }
 
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-    canvas.drawRect(size.toRect(), BasicPalette.red.paint());
+    canvas.drawRect(size.toRect(), BasicPalette.yellow.paint());
   }
+
+  Rect toRect() => Rect.fromLTWH(position.x, position.y, size.x, size.y);
 }

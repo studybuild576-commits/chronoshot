f2e011c9d29180b97b7fd637395d84a85dad2749
studybuild576-commits <studybuild576@gmail.com>
2025-09-13 15:36:49 +0530

    Create player.dart

---
+++ lib/components/player.dart
@@ -0,0 +1,64 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flame/palette.dart';
+import 'package:flutter/material.dart';
+
+import '../game.dart';
+import 'bullet.dart';
+import 'enemy.dart';
+
+class Player extends PositionComponent with HasGameRef<ChronoshotGame>, CollisionCallbacks {
+  final double _speed = 300;
+  final Timer _shootCooldown = Timer(0.5);
+  int health = 3;
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    size = Vector2.all(50.0);
+    position = gameRef.size / 2;
+    anchor = Anchor.center;
+    add(RectangleHitbox());
+    _shootCooldown.start();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    _shootCooldown.update(dt);
+
+    if (!gameRef.joystick.delta.isZero()) {
+      position.add(gameRef.joystick.delta.normalized() * _speed * dt);
+      position.clamp(Vector2.zero() + size / 2, gameRef.size - size / 2);
+    }
+
+    if (!gameRef.shootJoystick.delta.isZero() && _shootCooldown.finished) {
+      shoot();
+      _shootCooldown.start();
+    }
+  }
+
+  void shoot() {
+    final direction = gameRef.shootJoystick.delta.normalized();
+    final bullet = Bullet(
+      position: position + direction * 25,
+      direction: direction,
+    );
+    parent?.add(bullet);
+  }
+
+  @override
+  void onCollisionStart(Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollisionStart(intersectionPoints, other);
+    if (other is Enemy) {
+      health--;
+      other.removeFromParent();
+    }
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    canvas.drawRect(size.toRect(), BasicPalette.white.paint());
+  }
+}
